a6dc544c1dc9d37810b34261fc3d8f2d015f706b
indyfunder <panasuk238@gmail.com>
2021-03-12 13:53:27 +0700

    hw3

--- homework/ConsoleApp1/Program.cs
+++ homework/ConsoleApp1/Program.cs
@@ -6,62 +6,37 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            string agency;
-            int pass,one,ten,hun,k,tenk,hunk;
-            Console.Write("input password :");
-            pass = int.Parse(Console.ReadLine());
-            Console.Write("input Agency :");
-            agency = Console.ReadLine();
-            one = pass % 10;
-            ten = (pass % 100) / 10;
-            hun = (pass % 1000) / 100;
-            k = (pass % 10000) / 1000;
-            tenk = (pass % 100000) / 10000;
-            hunk= (pass % 1000000) / 100000;
-            if (agency == "CIA")
+
+            string mode; double x, y;
+            Console.Write("time/price : ");
+            mode = Console.ReadLine();
+            Console.Write("x : ");
+            x = double.Parse(Console.ReadLine());
+            Console.Write("y : ");
+            y = double.Parse(Console.ReadLine());
+
+            if (mode == "time")
             {
-                if (one % 3 == 0)
+                if (x! > 0)
                 {
-                    if (ten != 1 && ten != 3 && ten != 5) 
-                    {
-                        if (k >= 6 && k != 8)
-                        { Console.WriteLine("True"); }
-                        else { Console.WriteLine("False"); }
-                    }
-                    else { Console.WriteLine("False"); }
+                    y = (x - 1) * (x - 1);
+                    Console.WriteLine("{0}, {1}", x, y);
                 }
-                else { Console.WriteLine("False"); }                
+                else { Console.WriteLine("Invalid mouse position"); }
+
             }
-            if (agency == "FBI")
+            else if (mode == "price")
             {
-                if (hunk >= 4 && hunk <= 7)
+                if (x! > 0)
                 {
-                    if ( hun % 2 == 0 && hun != 6 )
-                    {
-                        if (tenk % 2 != 0)
-                        { Console.WriteLine("True"); }
-                        else { Console.WriteLine("False"); }
-                    }
-                    else { Console.WriteLine("False"); }
+                    x = Math.Sqrt(y) + 1;
+                    Console.WriteLine("{0}, {1}", x, y);
                 }
-                else { Console.WriteLine("False"); }
+                else { Console.WriteLine("Invalid mouse position"); }
             }
-            if (agency == "NSA")
-            {
-                if (30%one == 0)
-                {
-                    if (hun % 3 == 0 && hun % 2 != 0)
-                    {
-                        if (one==7 || ten==7 || hun==7 || k==7 || tenk==7 || hunk==7)
-                        { Console.WriteLine("True"); }
-                        else { Console.WriteLine("False"); }
-                    }
-                    else { Console.WriteLine("False"); }
-                }
-                else { Console.WriteLine("False"); }
-            }
-            else { Console.WriteLine("False"); }
+            else { Console.WriteLine("Invalid mode"); }
             Console.ReadLine();
-        }
+
+        }   
     }
 }
