d9426f89c8f38880961efe5868f41a87318e1cc3
indyfunder <panasuk238@gmail.com>
2021-03-28 11:19:31 +0700

    ข้อ 2

---
+++ homework/ข้อ 2/ข้อ 2/ข้อ 2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ข้อ_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, x, y,i;
+            Console.Write("input a :");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("input b :");
+            b = int.Parse(Console.ReadLine());
+            x = a;
+            y = b;
+            i = 0;
+            
+            while (i>=0)
+            {
+                if (x == y)
+                {
+                    Console.WriteLine(x);
+                    break;
+                }
+                else
+                {
+                    if (x < y)
+                    {
+                        x = x + a;
+                    }
+                    else
+                    {
+                        y = y + b;
+                    }
+                }
+                i++;
+            }
+
+
+
+
+        }
+    }
+}
